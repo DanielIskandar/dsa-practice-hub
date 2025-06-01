78bf883a658dd819cdd2b928f454c8d871a426cf
Daniel Iskandar <danisk@umich.edu>
2025-05-31 23:38:02 -0400

    Solved top_k_frequent_elements problem

--- Array-and-Hashing/top_k_frequent_elements_M.py
+++ Array-and-Hashing/top_k_frequent_elements_M.py
@@ -15,14 +15,19 @@ class Solution:
 
         hash_set [#, occurences]
         """
-        hash_set = []
+        hash_set = {}
         for num in nums:
             if num in hash_set:
                 hash_set[num] += 1
             else:
                 hash_set[num] = 1
 
-        hash_set.sort()
+        hash_list = list(hash_set.items())
+        hash_list.sort(key=lambda pair: pair[1], reverse=False)
+        sorted_keys = [key for key, value in hash_list]
+
+        value_to_slice = len(hash_list) - k
+        return sorted_keys[value_to_slice:]
 
 
 
@@ -32,3 +37,19 @@ class test_solution(unittest.TestCase):
         k = 2
         correct_answer = [2,3]
         self.assertEqual(Solution.topKFrequent(self, nums, k), correct_answer)
+    
+    def test_out_of_order(self):
+        nums = [3, 2, 1, 3, 2, 1, 3, 3, 2]
+        k = 2
+        correct_answer = [2, 3]
+        self.assertEqual(Solution.topKFrequent(self, nums, k), correct_answer)
+    
+    def test_negative_values(self):
+        nums = [-3, 2, 1, -3, 2, 1, -3, -3, 2]
+        k = 2
+        correct_answer = [2, -3]
+        self.assertEqual(Solution.topKFrequent(self, nums, k), correct_answer)
+
+
+if __name__ == "__main__":
+    unittest.main()
